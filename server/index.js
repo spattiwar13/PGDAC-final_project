65a34a0d4f98905e33d7ecbf83f702f0bdf9ea99
Shubham Pattiwar <spattiwar13@gmail.com>
2023-09-04 15:29:02 +0530

    hello

---
+++ server/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import adminRoutes from "./routes/adminRoutes.js";
+import studentRoutes from "./routes/studentRoutes.js";
+import facultyRoutes from "./routes/facultyRoutes.js";
+const app = express();
+dotenv.config();
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/api/admin", adminRoutes);
+app.use("/api/faculty", facultyRoutes);
+app.use("/api/student", studentRoutes);
+
+const PORT =  5000;
+app.get("/", (req, res) => {
+  res.send("Hello to college erp API");
+});
+
+const url="mongodb+srv://Group112:Cdac123@cluster0.7eijxwu.mongodb.net/cms?retryWrites=true&w=majority"
+
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  )
+  .catch((error) => console.log("Mongo Error", error.message));
